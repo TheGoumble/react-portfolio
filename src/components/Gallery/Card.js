f698da8260650a47fa2a3c47c3c8a6921921946b
Javier Vargas <3231javiervargas@gmail.com>
2023-04-29 22:24:30 -0400

    making different styles for all the project cards

--- src/components/Gallery/Card.js
+++ src/components/Gallery/Card.js
@@ -2,33 +2,37 @@ import { Col, Card } from "../../utilis/Bootstrap.js"
 import { Laptop, InfoCircleFill } from "react-bootstrap-icons"
 import { Link } from "react-router-dom"
 
-import styles from "./gallery.module.css"
+import "./gallery.css"
 
 export default function GalleryCard({
-  data: { id, title, tech, language, cover, content, website, info },
+  data: { id, title, tech, language, website, style },
 }) {
   return (
-    <Col key={id} className="button-effect project-card">
-      <Card>
-        <article className={`card-body p-3 ${styles.projects}`}>
+    <Col key={id} className="button-effect">
+      <Card className={`${style}`}>
+        <article className="card-body p-3 d-flex align-content-between justify-content-center flex-wrap projects">
           <header>
             <h2 className="d-flex justify-content-center">
               <a href={website} target="_blank" rel="noreferrer">
                 {title}
               </a>
             </h2>
-            <p className="mb-0 pb-0 author d-flex justify-content-center">{language}</p>
-            <p className="mt-0 pt-0 prior d-flex justify-content-center">
-              <small>{tech}</small>
-            </p>
+            <div>
+              <p className="mb-0 pb-0 author d-flex justify-content-center">
+                {language}
+              </p>
+              <p className="mt-0 pt-0 prior text-center">
+                <small>{tech}</small>
+              </p>
+            </div>
           </header>
-          <div className="d-flex justify-content-around">
+          <div className="d-flex justify-content-around w-100">
             <Link to={`${website}`}>
-              <Laptop color="#2ab2ae" size={27} /> Website
+              <Laptop size={27} /> Website
             </Link>
-            <a href={`/${title}`}>
-              <InfoCircleFill color="#2ab2ae" size={27} /> about
-            </a>
+            <Link to={`/${title}`}>
+              <InfoCircleFill size={27} /> about
+            </Link>
           </div>
         </article>
       </Card>
--- src/pages/Info/Info.js
+++ src/pages/Info/Info.js
@@ -1,21 +1,24 @@
-
 import data from "../../data/data.json"
 import { useParams } from "react-router-dom"
 
+export default function Info() {
+  const { name } = useParams()
+  let siteData = {}
 
-export default function Info(){
-    const {name} = useParams()
-    let siteData = {}
-    
-    for(let i = 0; i <= data.length; i++){
-        if( name === data[i].title){
-            siteData = data[i]
-            console.log(data[i])
-            break
-        }
+  for (let i = 0; i <= data.length; i++) {
+    if (name === data[i].title) {
+      siteData = data[i]
+      console.log(data[i])
+      break
     }
+  }
 
-    return( 
+  return (
+    <section>
       <h2>This site stuff {name}</h2>
-    )
+      <p>{siteData.language}</p>
+      <p>{siteData.tech}</p>
+      <h4>{siteData.content}</h4>
+    </section>
+  )
 }
